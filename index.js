b9db7011ef6cc029795a173b25ee8a5815eba08f
apophis30 <kumaramanpur30nov@gmail.com>
2024-12-19 20:43:32 +0000

    fixed index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const rootRouter = require("./routes/index.js")
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const PORT = 3000;
 
 const app = express();
 
+app.use(cors);
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
 app.use('/api/v1', rootRouter);
+
+app.listen(PORT, (err) => {
+    if(err) console.log(`Error in server setup: ${err}`);
+    console.log(`Server running on: ${PORT}`);
+})
